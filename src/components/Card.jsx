7b30c40a17e2dccf9e66b3cb9eb64cc17b9c92dd
Talha-TN <nawazt964@gmail.com>
2024-12-03 13:19:32 -0800

    comments added for daud

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,10 @@ import React from "react";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import { useSelector } from "react-redux";
 import "react-loading-skeleton/dist/skeleton.css"; // Import Skeleton CSS
-
 const Card = () => {
   const { moviesData, isLoading } = useSelector((state) => state.MoviesSlice);
 
   if (isLoading) {
-    // Display skeleton loaders for cards
     return (
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
         {Array.from({ length: 16 }).map((_, index) => (
@@ -30,8 +28,7 @@ const Card = () => {
     );
   }
 
-  // Display movie cards if data is available
-  if (!moviesData || !moviesData.results) {
+  if (!moviesData || !moviesData.results) {   // Display movie cards if data is available
     return <p className="text-lg mt-2 ">click the button to fetch movies</p>;
   }
 
--- src/slices/MoviesSlice.jsx
+++ src/slices/MoviesSlice.jsx
@@ -1,12 +1,8 @@
 
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-
-// Fetch movies data asynchronously with async/await and a delay
-export const fetchMovies = createAsyncThunk("fetchMovies", async () => {
+export const fetchMovies = createAsyncThunk("fetchMovies", async () => { // Fetch movies data asynchronously with async/await and a delay
   try {
-    // Simulate API delay with setTimeout
     await new Promise((resolve) => setTimeout(resolve, 2000));
-
     const response = await fetch(
       "https://api.themoviedb.org/3/movie/popular?api_key=97b85a3e493dbe5c6ec0337641ae6907"
     );
@@ -19,18 +15,18 @@ export const fetchMovies = createAsyncThunk("fetchMovies", async () => {
     return data;
   } catch (error) {
     console.error("Error fetching movies:", error.message);
-    throw error; // Propagate error to handle it in the reducer
+    throw error; 
   }
 });
 
 const MoviesSlice = createSlice({
   name: "MoviesSlice",
   initialState: {
-    moviesData: {}, // Use an object to hold API response
+    moviesData: {},
     isLoading: false,
     error: null,
   },
-  extraReducers: (builder) => {
+  extraReducers: (builder) => {     // biuilders to listen to async function
     builder
       .addCase(fetchMovies.pending, (state) => {
         state.isLoading = true;
@@ -38,11 +34,11 @@ const MoviesSlice = createSlice({
       })
       .addCase(fetchMovies.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.moviesData = action.payload; // Store the API response
+        state.moviesData = action.payload; 
       })
       .addCase(fetchMovies.rejected, (state, action) => {
         state.isLoading = false;
-        state.error = action.error.message; // Store the error message
+        state.error = action.error.message; 
       });
   },
 });
